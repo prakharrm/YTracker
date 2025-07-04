3aa0eba277118fad120f9a4177507e737c1e6b47
Prakhar Mahant <prakharmahant@gmail.com>
2025-07-05 02:07:55 +0530

    vercel domain cors

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,22 @@
 import express from "express";
-import dotenv from "dotenv"
-import cors from "cors"
-import playlist from "./modules/playlist.js"
+import dotenv from "dotenv";
+import cors from "cors";
+import playlist from "./modules/playlist.js";
 
 dotenv.config();
 
 const app = express();
 
-
 app.use(cors());
 app.use(
-    cors({
-      origin: "http://localhost:3000", 
-      credentials: true,
-    })
-  );
+  cors({
+    origin: ["http://localhost:3000", "https://y-tracker.vercel.app/"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use("/api", playlist)
+app.use("/api", playlist);
 
-app.listen(5000, ()=> {
-    console.log("app running on port ", 5000);
-})
+app.listen(5000, () => {
+  console.log("app running on port ", 5000);
+});
